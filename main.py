7643caec3b9b85064f05f577e4c7d9f91574be4a
Punit Soneji <punit.soneji@hotmail.com>
2021-08-18 19:56:15 +0530

    added the DB connectivity

--- main.py
+++ main.py
@@ -1,2 +1,13 @@
 write your code here
 include all the files
+engine = sal.create_engine('mssql+pymssql://MSAD\sonejpun:hello@server:port12/db_name')
+engine = sal.create_engine('mssql+pymssql://MSAD\sonejpun:hello@server:port12/db_name')
+engine = sal.create_engine('mssql+pymssql://MSAD\sonejpun:hello@server:port12/db_name')
+hello@server
+helloworld
+
+engine = sal.create_engine('mssql+pymssql://MSAD\sonejpun:hello@server:port12/db_name')
+
+
+engine = sal.create_engine('mssql+pymssql://MSAD\sonejpun:hello@server:port12/db_name')
+
