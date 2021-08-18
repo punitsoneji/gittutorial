39078fc18db3c510b5bcd2d24daf6cb71d69dda2
Punit Soneji <punit.soneji@hotmail.com>
2021-08-18 19:56:50 +0530

    removed the DB connectivity

--- main.py
+++ main.py
@@ -1,13 +1,2 @@
 write your code here
 include all the files
-engine = sal.create_engine('mssql+pymssql://MSAD\sonejpun:hello@server:port12/db_name')
-engine = sal.create_engine('mssql+pymssql://MSAD\sonejpun:hello@server:port12/db_name')
-engine = sal.create_engine('mssql+pymssql://MSAD\sonejpun:hello@server:port12/db_name')
-hello@server
-helloworld
-
-engine = sal.create_engine('mssql+pymssql://MSAD\sonejpun:hello@server:port12/db_name')
-
-
-engine = sal.create_engine('mssql+pymssql://MSAD\sonejpun:hello@server:port12/db_name')
-
